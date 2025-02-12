3ea644e915c4659a3c0726d36b024b0d4e7e06a4
Alessandro Schiavo <aschiavo@minsait.com>
2025-02-13 00:24:46 +0100

    modificato script per la generazione della mappa delle bboxes

--- src/utils/write_bbox_map.py
+++ src/utils/write_bbox_map.py
@@ -1,6 +1,12 @@
 import folium
-import json
-from src.dataminer.bounding_boxes import BOUNDING_BOXES
+import os
+import sys
+
+# Aggiungi il percorso della cartella "utils" al PYTHONPATH
+utils_path = os.path.join(os.path.dirname(__file__), "..")
+sys.path.append(utils_path)
+
+from dataminer.bounding_boxes import BOUNDING_BOXES
 
 mappa = folium.Map(location=[42.0, 12.5], zoom_start=6)
 
@@ -11,7 +17,21 @@ for regione, dati in BOUNDING_BOXES.items():
         [dati["ur_lat"], dati["ur_lon"]],  # Upper right
         [dati["ll_lat"], dati["ur_lon"]],  # Lower right
         [dati["ll_lat"], dati["ll_lon"]]   # Closing the box
-    ]    
+    ]
+    
     folium.PolyLine(box, color="blue", weight=2.5).add_to(mappa)
 
+    center_lat = (dati["ll_lat"] + dati["ur_lat"]) / 2
+    center_lon = (dati["ll_lon"] + dati["ur_lon"]) / 2
+
+    folium.Marker(
+        location=[center_lat, center_lon],
+        icon=folium.DivIcon(
+            icon_size=(50, 20),
+            icon_anchor=(25, 10),
+            html=f'<div style="font-size: 12pt; color: blue; border-radius: 3px; padding: 2px;">{regione}</div>'
+        )
+    ).add_to(mappa)
+
 mappa.save("public/bounding_boxes_italia.html")
+print("Mappa salvata in public/bounding_boxes_italia.html")
